a84da510615a386f83f3ca5c074aff3c1d9dff85
Ashutosh Pandey <155779632+Pandey-A@users.noreply.github.com>
2025-01-16 02:56:07 +0530

    bug fixes

---
+++ vite.config.js
@@ -0,0 +1,9 @@
+export default {
+    build: {
+      outDir: 'dist', // Ensure this matches your deployment setup
+    },
+    server: {
+      historyApiFallback: true, // Optional for development
+    },
+  };
+  
