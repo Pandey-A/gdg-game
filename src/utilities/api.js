e8c7dbc90613c29bf023400960e14f5508d302fa
Ashutosh Pandey <155779632+Pandey-A@users.noreply.github.com>
2025-01-16 02:50:27 +0530

    for deployment

--- src/utilities/api.js
+++ src/utilities/api.js
@@ -1,15 +1,15 @@
-// // New api
+
+// try 1
+
+// New api
 // const url =
 //   "https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/LOwAyldXGa2Rle8qWWi7/scores";
 
-// const username = localStorage.getItem("username");
-// const teamScore = localStorage.getItem("teamScore");
-
 // // Submit data to the Api
-// export const postScore = async (username, teamScore) => {
+// export const postScore = async () => {
 //   const data = {
-//     user: username,
-//     teamScore,
+//     user: localStorage.getItem("username"),
+//     teamScore: localStorage.getItem("teamScore"),
 //   };
 
 //   const response = await fetch(url, {
@@ -17,6 +17,36 @@
 //     cache: "no-cache",
 //     headers: {
 //       "Content-Type": "application/json",
+//     },
+//     body: JSON.stringify(data),
+//   });
+
+//   return response.json();
+// };
+
+// const url = 'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/LOwAyldXGa2Rle8qWWi7/scores';
+
+// // Submit data to the Api
+// export const postScore = async () => {
+//   // Retrieve the teamName and teamScore from localStorage
+//   const teamName = localStorage.getItem('teamName');
+//   const teamScore = localStorage.getItem('teamScore');
+
+//   // Check if the values exist in localStorage
+//   if (!teamName || !teamScore) {
+//     return 'Error: Missing team name or score in localStorage.';
+//   }
+
+//   const data = {
+//     user: teamName, // Using teamName as the user
+//     score: parseInt(teamScore), // Convert teamScore to integer
+//   };
+
+//   const response = await fetch(url, {
+//     method: 'POST',
+//     cache: 'no-cache',
+//     headers: {
+//       'Content-Type': 'application/json',
 //     },
 //     body: JSON.stringify(data),
 //   });
@@ -36,28 +66,3 @@
 //   const data = await response.json();
 //   return data.result;
 // };
-
-// try 1
-
-// New api
-const url =
-  "https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/LOwAyldXGa2Rle8qWWi7/scores";
-
-// Submit data to the Api
-export const postScore = async () => {
-  const data = {
-    user: localStorage.getItem("username"),
-    teamScore: localStorage.getItem("teamScore"),
-  };
-
-  const response = await fetch(url, {
-    method: "POST",
-    cache: "no-cache",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify(data),
-  });
-
-  return response.json();
-};
